553b7e1e2dd7f14f279edfcc7ec7097c6a56c774
Andrew Kamau <andkamau@gmail.com>
2016-08-10 13:46:08 +0300

    add import list members functionality

--- scripts/lists.py
+++ scripts/lists.py
@@ -9,7 +9,7 @@ Usage:
     python scripts/lists.py list 763292547754131456
 
 """
-import sys
+import sys, time
 from pylitwoops.streaming import listener, config
 
 
@@ -34,6 +34,17 @@ def list_members(list_id):
     members = t.list_members(list_id=int(list_id), count=500)
     for member in members:
         print "%s :: @%s" % (member.id, member.screen_name)
+    print "%s members" % len(members)
+    return members
+
+
+def list_members_by_name(owner_name, list_slug):
+    t = listener.get_api()
+    _list = t.get_list(owner_screen_name=str(owner_name), slug=str(list_slug))
+    members = t.list_members(owner_screen_name=str(owner_name), slug=str(list_slug), count=500)
+    for member in members:
+        print "%s :: @%s" % (member.id, member.screen_name)
+    print "%s members in %s's list '%s' list_id: %s" % (len(members), owner_name, list_slug, _list.id)
     return members
 
 
@@ -47,11 +58,31 @@ def refresh(list_id):
     r.save()
 
 
+def _import(from_list_id, to_list_id):
+    t = listener.get_api()
+    from_list = t.get_list(list_id=int(from_list_id))
+    to_list = t.get_list(list_id=int(to_list_id))
+    print "Importing members of %s's list '%s' to %s's list '%s'..." % (
+            from_list.user.screen_name,
+            from_list.name,
+            to_list.user.screen_name,
+            to_list.name
+            )
+    time.sleep(4)
+    from_members = list_members(int(from_list_id))
+    to_members = []
+    for member in from_members:
+        to_members.append(member.id)
+    add_list_member(int(to_list_id), to_members)
+    print "imported %s members from %s to %s" % (len(from_members), from_list_id, to_list_id)
 
 
 if __name__ == "__main__":
-    actions = ['create', 'add', 'list']
+    actions = ['create', 'add', 'list_by_id', 'list_by_name', 'refresh', 'import']
     action = sys.argv[1]
+    if action not in actions:
+        print "Unknown action"
+        sys.exit(2)
     if action == 'create':
         list_name = sys.argv[2]
         mode = sys.argv[3]
@@ -62,9 +93,17 @@ if __name__ == "__main__":
         for _id in sys.argv[3].split(","):
             members.append(int(_id))
         add_list_member(list_id, members)
-    elif action == 'list':
+    elif action == 'list_by_id':
         list_id = sys.argv[2]
         list_members(int(list_id))
+    elif action == 'list_by_name':
+        owner_name = sys.argv[2]
+        list_slug = sys.argv[3]
+        list_members_by_name(owner_name, list_slug)
     elif action == 'refresh':
         list_id = sys.argv[2]
         refresh(list_id)
+    elif action == 'import':
+        from_list_id = sys.argv[2]
+        to_list_id = sys.argv[3]
+        _import(from_list_id, to_list_id)
