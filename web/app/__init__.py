e0f0aba610155d60694d6abd8eff5e7961b5f384
mevey <muthoni90@gmail.com>
2016-09-07 16:15:14 +0300

    Fixed githu link conflict

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -65,9 +65,9 @@ def tracked_users():
     '''
     redis_client = get_redis()
     users = []
-    page = request.args.get('page', type=int, default=1)
+    page = request.args.get('page', type=int, default=1) - 1
     per_page = 18
-    for user in redis_client.keys("%s*" % app.config['PREFIX']['user']):
+    for user in redis_client.keys("%s*" % app.config['PREFIX']['user'])[page * per_page: page * per_page + per_page]:
         user_payload = eval(redis_client.get(user))
 
         if not str(user_payload['id']) == app.config['HEARTBEAT_ACCOUNT']:
@@ -78,8 +78,8 @@ def tracked_users():
                 bio=user_payload['description']
                 ))
     pagination = Pagination(page=page, total=len(users), search='', record_name='users')
-    pagecount = int(math.ceil( float(len(users))/per_page))
-    return render_template('users.html', users=users[page * per_page - per_page: page * per_page], pagination=pagination, pagecount=pagecount, page=page)
+    pagecount = int(math.ceil( float(len(redis_client.keys("%s*" % app.config['PREFIX']['user'])))/per_page))
+    return render_template('users.html', users=users, pagination=pagination, pagecount=pagecount, page=page)
 
 
 
