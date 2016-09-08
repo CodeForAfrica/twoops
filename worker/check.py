7ce8987c181bb47cf857ee99edfa9117578c011c
Andrew Kamau <andkamau@gmail.com>
2016-09-08 13:01:37 +0300

    track run duration for deletion checker

--- worker/check.py
+++ worker/check.py
@@ -31,6 +31,7 @@ def chunkify(list_, size):
 def main():
     """
     """
+    start_time = datetime.datetime.now()
     redis_client = get_redis()
     entries = redis_client.keys("%s*" % PREFIX['new'])
 
@@ -68,8 +69,9 @@ def main():
                 print "Unexpected response for %s -- %s" % (entry, other_error)
 
 
+        duration = datetime.datetime.now() - start_time
         now = time.asctime() + '|' + str(delete_count)
-        print "Last update: %s | Last run delete count: %s" % (now, delete_count)
+        print "Last update: %s | Last run delete count: %s | Duration: %s seconds" % (now, delete_count, duration.seconds)
         redis_client.set(TIME_KEY, now)
 
 if __name__ == '__main__':
