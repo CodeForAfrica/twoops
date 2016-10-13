656722508d8ffe9ddae3d90751a2631776bdab67
Andrew Kamau <andkamau@gmail.com>
2016-10-13 13:56:43 +0300

    we need the IDs as well

--- scripts/dumpdata.py
+++ scripts/dumpdata.py
@@ -10,12 +10,14 @@ def export_deleted_tweets():
     redis_client = get_redis()
     with open(config.EXPORT_FILE, 'w') as outputfile:
         csvwriter = csv.writer(outputfile)
-        csvwriter.writerow(["user", "text"])
+        csvwriter.writerow(["id", "user", "text"])
         for tweet in redis_client.keys(config.PREFIX["deleted"] + "*"):
             try:
                 tweet_payload = eval(redis_client.get(tweet))
                 csvwriter.writerow(
-                        [tweet_payload.get("username"), tweet_payload.get("message")] )
+                        [tweet_payload.get("request_id"),
+                            tweet_payload.get("username"),
+                            tweet_payload.get("message")] )
             except Exception, err:
                 print "Failed for %s -- %s" % (tweet, err)
 
